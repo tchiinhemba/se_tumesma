87684a032d022e2586ff3c89fcbbed280e0aef5a
Eládio Tchiinhemba <tchiinhemba@gmail.com>
2021-02-20 08:26:16 +0100

    update

--- public/scripts/setup.js
+++ public/scripts/setup.js
@@ -2,7 +2,8 @@ var owl = $('.owl-carousel');
 owl.owlCarousel({
     items:5,
     loop:true,
-    margin:8,
+    margin: 10,
+    dots: false,
     autoplay:true,
     autoplayTimeout:2500,
     autoplayHoverPause:true
